455c0f64734a1279ff4e90107a10a47c22fcb8c0
Ivo Beerens <ibeerens@gmail.com>
2021-07-07 10:39:16 +0200

    Update set-loc-vmwtools.ps1

--- set-loc-vmwtools.ps1
+++ set-loc-vmwtools.ps1
@@ -7,18 +7,20 @@ $VCserver = read-host "Enter the vCenter server name"
 # Connect to the vCenter server 
 Connect-VIServer -server $VCserver
 
+# variable
 $cluster = 'CL-MGNT'
 $hosts = Get-Cluster -Name $cluster | Get-VMHost
 $datastore = '/vmfs/volumes/nfs01/vmwtools/11.3.0-18090558/'
 
-# Display current VMware Location
-$hosts | Get-AdvancedSetting -Name "UserVars.ProductLockerLocation" | Select-Object Entity,Value
+# Display the current VMware Tools location
+# default is: /locker/packages/vmtoolsRepo/
+$hosts | Get-AdvancedSetting -Name "UserVars.ProductLockerLocation" | Select-Object Entity, Value
 
-# Change VMware Tools location 
+# Change the VMware Tools location
 Get-cluster -name $cluster | Get-VMhost | %{$_.ExtensionData.UpdateProductLockerLocation($datastore)}  
 
 # Display current VMware Location
 $hosts | Get-AdvancedSetting -Name "UserVars.ProductLockerLocation" | Select-Object Entity,Value
 
-# Disconnect vCenter 
+# Disconnect from vCenter 
 Disconnect-VIserver -server * -Confirm:$false
