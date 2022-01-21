2f52d25cb3753a6b6841233a5c953dc077f304fd
Ivo Beerens <info@ivobeerens.nl>
2022-01-21 15:18:09 +0100

    update

--- Horizon-Highest-users.ps1
+++ Horizon-Highest-users.ps1
@@ -1,27 +1,30 @@
 #
 # VMware Horizon Display the highest user count and perform a counter reset
-# 
+# https://github.com/vmware/PowerCLI-Example-Scripts 
+# $env:PSModulePath
 # Connect to VMware Horizon
-Write-Output "", "Connecting to the Horizon Connection Server" 
-Connect-HVServer -Server <server> -user <user> -domain <domain>
+
+# Import Modules
+Import-Module -Name VMware.VimAutomation.HorizonView 
 
 # Variables
+$hor_server = "vdi.ivobeerens.nl"
+$hor_domain = "lab.local"
 $date = Get-date -UFormat "%d-%m-%Y"
-$output = "c:\Scripts\Output\CCU\horizon-ccu-$date.txt"
-$smtp = " "
-$mailto = " "
-$mailfrom = " "
-$subject = " "
+$file_output = "c:\temp\CCU\horizon-ccu-$date.txt"
+
+Write-Output "", "Connecting to the Horizon Connection Server" 
+Connect-HVServer -Server $hor_server -domain $hor_domain
 
 # Horizon API overview
 $services1 = $Global:DefaultHVServers.ExtensionData
 
-# Get the highest licensing
+# Get the highest usage
 $getusage = $services1.UsageStatistics.UsageStatistics_GetLicensingCounters()
 $totalccu = $getusage.HighestUsage.TotalConcurrentConnections
 
 # Write to file
-Write-Output The highest usage count is: $totalccu" | Out-File $output -Force
+Write-Output "The highest usage count is: $totalccu" | Out-File $file_output -Force
 
 # Mail
 Send-MailMessage -SMTPServer $smtp -To $mailto -From $mailfrom -Subject $subject -body "$date, The highest usage count is: $totalccu"
